150ac728179b739329a19602a0adb6607c6caedd
Sohima <54314395+Sohima@users.noreply.github.com>
2021-09-30 09:37:34 +0530

    Update sudoku_ai.py

--- sudoku_ai.py
+++ sudoku_ai.py
@@ -10,23 +10,35 @@ Original file is located at
 
 In this project, an AI Agent has been created which utilizes backtracking search and Constraint Satisfaction Problem heuristics to solve Sudoku puzzles of variable size.
 
-A sudoku board is comprised of a grid of size N, with each grid comprised of p rows and q columns. The agent will attempt backtracking search to fill the grid cells so that each row, column, and grid cell has unique numbers.
+A sudoku board is comprised of a grid of size N, with each grid comprised of p rows and q columns. The agent will attempt backtracking search to fill the grid cells so that
+each row, column, and grid cell has unique numbers.
 
-This is a program that solves sudoku puzzles using artificial intelligence concepts. This program treats the puzzle as a Constraint Satisfaction Problem (CSP). Each of the squares in the puzzle is treated as a separate variable, with domain 1-9. The constraints to the problem are that each row, column, and 3x3 sub-square must contain the numbers 1 through 9 with no repeats.
+This is a program that solves sudoku puzzles using artificial intelligence concepts. This program treats the puzzle as a Constraint Satisfaction Problem (CSP). 
+Each of the squares in the puzzle is treated as a separate variable, with domain 1-9. The constraints to the problem are that each row, column, and 3x3 sub-square must 
+contain the numbers 1 through 9 with no repeats.
 
-The way this problem is solved is using the Backtracking Search algorithm. This is a variation on Depth First Search (DFS) that eliminates states that are invalid before they are expanded to completion in order to reduce the number of expanded nodes and therefore increase performance. Also the different variables are expanded in an order such that it will limit the number of nodes that have to be expanded.
+The way this problem is solved is using the Backtracking Search algorithm. This is a variation on Depth First Search (DFS) that eliminates states that are invalid before 
+they are expanded to completion in order to reduce the number of expanded nodes and therefore increase performance. Also the different variables are expanded in an order
+such that it will limit the number of nodes that have to be expanded.
 
-In order to eliminate as many contradictory possible values as possible, as quickly as possible, this CSP implements the Arc-Consistency: Constraint Propagation algorithm, or AC-3. Whenever a value is updated it will form a queue of arcs, which are pairs of positions that are affected by the updated value. Then it will go through each arc and remove values from the domain of the tail of each arc if they contradict. If a domain is altered it will add all of the arcs affected by this domain change to the queue and will repeat until the queue is empty.
+In order to eliminate as many contradictory possible values as possible, as quickly as possible, this CSP implements the Arc-Consistency: Constraint Propagation algorithm, 
+or AC-3. Whenever a value is updated it will form a queue of arcs, which are pairs of positions that are affected by the updated value. Then it will go through each arc and
+remove values from the domain of the tail of each arc if they contradict. If a domain is altered it will add all of the arcs affected by this domain change to the queue and 
+will repeat until the queue is empty.
 
-The possible values of each variable are kept track of in the Sudoku class instances and when it comes time to decide which variable to expand next in the search, it will use the given heuristic to determine which variable to expand next. The heuristics that are implemented in this program are as follows:
+The possible values of each variable are kept track of in the Sudoku class instances and when it comes time to decide which variable to expand next in the search, it will
+use the given heuristic to determine which variable to expand next. The heuristics that are implemented in this program are as follows:
 
 The trivial heuristic.
 This heuristic returns the first variable that has not yet been expanded.
 This does not help performance in any way.
 The Minimum Remaining Values (MRV) heuristic.
 This heuristic chooses the node with the least amount of possible values remaining.
-This helps reduce the total number of nodes expanded because there are fewer branches to the search tree this way, and it is more likely to pick the correct value for the given variable.
-A state is discarded if it is found to be in violation of any of the constraints of the sudoku puzzle. If there is more than one of the same number in any given row, column, or 3x3 sub-square then that state is discarded so that it does not have to keep being expanded until it is complete to find out that it is incorrect. In addition if a variable's domain is empty, then that state is discarded because there is no possible value that can be entered there such that the state maintains validity.
+This helps reduce the total number of nodes expanded because there are fewer branches to the search tree this way, and it is more likely to pick the correct value for
+the given variable.
+A state is discarded if it is found to be in violation of any of the constraints of the sudoku puzzle. If there is more than one of the same number in any given row,
+column, or 3x3 sub-square then that state is discarded so that it does not have to keep being expanded until it is complete to find out that it is incorrect. In addition 
+if a variable's domain is empty, then that state is discarded because there is no possible value that can be entered there such that the state maintains validity.
 """
 
 from time import time
